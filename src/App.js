96d952b50af386a01ad2139265e241b21a906e43
radixdt2212 <85940721+radixdt2212@users.noreply.github.com>
2021-06-18 13:13:34 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class Form extends Component {
+  state = {
+    ID: 0,
+    firstName: "",
+    lastName: "",
+    image: "",
+    CollegeName: "",
+    studentList: [],
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleImageChange = (e) => {
+    const imgName = e.target.files[0].name;
+    this.setState({ image: imgName });
+    e.target.value = null;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const studentInfo = ` ${this.state.ID} ${this.state.firstName} ${this.state.lastName} ${this.state.image} ${this.state.CollegeName} `;
+    this.setState({
+      studentList: [...this.state.studentList, studentInfo],
+      ID: 0,
+      firstName: "",
+      lastName: "",
+      image: "",
+      CollegeName: "",
+    });
+  };
+  render() {
+    return (
+      <>
+        <section style={{ margin: "3rem", fontSize: "2rem" }}>
+          <img
+            src="https://tutorials.rxtrainees.radixweb.net/wp-content/uploads/2020/12/logo_2500X1875-2048x1536.jpg"
+            height="150px"
+            width="150px"
+            alt="College Logo"
+          />
+          <input
+            type="number"
+            name="ID"
+            placeholder="ID"
+            value={this.state.ID}
+            onChange={this.handleChange}
+          ></input>
+          <input
+            type="text"
+            value={this.state.firstName}
+            name="firstName"
+            placeholder="First Name"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            type="text"
+            value={this.state.lastName}
+            name="lastName"
+            placeholder="Last Name"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            type="text"
+            value={this.state.CollegeName}
+            name="CollegeName"
+            placeholder="College Name"
+            onChange={this.handleChange}
+          ></input>
+          <span style={{ fontSize: "1rem" }}>
+            Image:
+            <input
+              type="file"
+              name="img"
+              placeholder="file"
+              onChange={this.handleImageChange}
+              accept="image/png, image/gif, image/jpeg"
+            ></input>
+          </span>
+          <button
+            type="submit"
+            className="btnFancy"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </button>
+          <div>
+            Student List :
+            <ul>
+              {this.state.studentList.map((student, index) => (
+                <li key={"student" + index}>{student}</li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    return <Form />;
+  }
+}
